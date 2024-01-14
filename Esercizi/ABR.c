a79c6554432fe05b2c8c3e2b2d1acdc080e98cae
bickpenna <mario.penna00@gmail.com>
2024-01-14 16:27:34 +0100

    Esercizio ABR

--- Esercizi/ABR.c
+++ Esercizi/ABR.c
@@ -9,6 +9,8 @@ struct Nodo{
     link dx;
 };
 
+// ABR
+
 link newNodo(int dato){
     link new = (link) malloc(sizeof(struct Nodo));
 
@@ -36,6 +38,15 @@ link minABR(link root){
     return root;
 }
 
+void printInOrder(link root){
+    if(root){
+        printInOrder(root->sx);
+        printf("%d ", root->dato);
+        printInOrder(root->dx);
+    }
+}
+
+// 1) Elimina elemento
 void delNodo(link *root, int dato){
     if((*root) == NULL) return;
 
@@ -68,26 +79,56 @@ void delNodo(link *root, int dato){
     }
 }
 
-void printInOrder(link root){
-    if(root){
-        printInOrder(root->sx);
-        printf("%d ", root->dato);
-        printInOrder(root->dx);
-    }
+// 2) Rimozione elementi dispari
+void delOdd(link *root){
+    if (*root == NULL) return;
+
+    delOdd(&(*root)->sx);
+    delOdd(&(*root)->dx);
+
+    if ((*root)->dato % 2 != 0)
+        delNodo(root, (*root)->dato);
+}
+
+// 3) Albero con valore nodi raddoppiati
+void abrRaddoppiato(link *newRoot, link root){
+    if (root == NULL) return;
+
+    abrRaddoppiato(newRoot, root->sx);
+    abrRaddoppiato(newRoot, root->dx);
+
+    insertABR(newRoot, root->dato * 2);
+}
+
+link retAbrRaddoppiato(link root){
+    link newRoot = NULL;
+    abrRaddoppiato(&newRoot, root);
+    return newRoot;
 }
 
 int main(){
     // ABR 1
     link root = NULL;
     int dim = 5;
-    srand(10);
+    srand(15);
 
     for(int i = 0; i<dim; i++)
         insertABR(&root, rand()%100);
     printInOrder(root);
 
+    // 1) Elimina Elemento
     int del = 70;
     delNodo(&root, del);
     printf("\nSenza %d: ", del);
     printInOrder(root);
+
+    // 2) Elimina Dispari
+    delOdd(&root);
+    printf("\nSenza Nodi Dispari: ");
+    printInOrder(root);
+
+    // 3) Valore Nodi Raddoppiati
+    link doubleRoot = retAbrRaddoppiato(root);
+    printf("\nCon nodi raddoppiati: ");
+    printInOrder(doubleRoot);
 }
